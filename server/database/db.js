09708a6b69c46d0bb98cdf38564f9d3b40309c8c
Nimish Lakhmani <nimishlakhmani@gmail.com>
2023-11-21 19:30:56 +0530

    commit

--- server/database/db.js
+++ server/database/db.js
@@ -7,7 +7,7 @@ const DBConnection = async () => {
     // const USERNAME = process.env.DB_USERNAME;
     // const PASSWORD = process.env.DB_PASSWORD;
 
-    const MONGO_URI = `mongodb+srv://nimishlakhmani:Bhulgye100@cluster0.gldd6qo.mongodb.net/?retryWrites=true&w=majority`;
+    
     try {
         await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
         console.log('Database connected successfully');
